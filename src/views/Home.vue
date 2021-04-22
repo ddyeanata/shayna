17164951550df607fd40d60d97a563f837f51aba
ddyeanata <naimjoe101@gmail.com>
2021-04-22 23:38:00 +0700

    Upload Project Shayna

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="home">
+      <HeaderShayna />
+      <HeroShayna />
+      <WomenShayna />
+      <InstaShayna />
+      <PartnerShayna />
+      <FooterShayna />
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from "@/components/HeaderShayna";
+import HeroShayna from "@/components/HeroShayna";
+import WomenShayna from "@/components/WomenShayna";
+import InstaShayna from "@/components/InstaShayna";
+import PartnerShayna from "@/components/PartnerShayna";
+import FooterShayna from "@/components/FooterShayna";
+
+
+export default {
+      name: "Home",
+      components: {
+            HeaderShayna,
+            HeroShayna,
+            WomenShayna,
+            InstaShayna,
+            PartnerShayna,
+            FooterShayna,
+      }
+};
+</script>
